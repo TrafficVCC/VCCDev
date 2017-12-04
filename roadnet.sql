e648b4fbc1bba7e1a2ab3f462b89127ee50da8ce
xinyuexy <1151980485@qq.com>
2017-12-04 21:12:03 +0800

    道路表

---
+++ roadnet.sql
@@ -0,0 +1,30 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50719
+Source Host           : localhost:3306
+Source Database       : vcc
+
+Target Server Type    : MYSQL
+Target Server Version : 50719
+File Encoding         : 65001
+
+Date: 2017-12-04 21:02:53
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for roadnet
+-- ----------------------------
+DROP TABLE IF EXISTS `roadnet`;
+CREATE TABLE `roadnet` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `lng` double NOT NULL,
+  `lat` double NOT NULL,
+  `distance` double DEFAULT NULL,
+  `way` varchar(40) NOT NULL,
+  `ld` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
